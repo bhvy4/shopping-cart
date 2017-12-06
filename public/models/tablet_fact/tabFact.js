45dc4d02b1d7939c41160600fa62098370947038
thelazydude <khanna.micky01@gmail.com>
2017-12-06 18:29:36 +0530

    minor changes

--- public/models/tablet_fact/tabFact.js
+++ public/models/tablet_fact/tabFact.js
@@ -1,7 +1,7 @@
 app.factory("tabfactory", function ($http, $q) {
     var getJSON = function () {
         var pr = $q.defer();
-        $http.get("http://localhost:7777/tablets").then(function (data) {
+        $http.get("https://shopping-cart-bhavya.herokuapp.com/tablets").then(function (data) {
             console.log("Success data");
             pr.resolve(data);
         }, function (er) {
